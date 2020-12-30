09474249a8dcd3d9a656842eaa01b59cc0005a38
anuraj <anurajguha@gmail.com>
2020-12-31 00:59:58 +0530

    update navbar and layout with materialize-css

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
-    return (
-        <>
-        <nav className="nav-extended teal darken-4">
+class Header extends Component {
+
+    componentDidMount() {
+        const M = window.M
+        document.addEventListener('DOMContentLoaded', function() {
+            const elems = document.querySelectorAll('.sidenav')
+            const instances = M.Sidenav.init(elems, {})
+          })
+    }
+
+    render() {
+        return (
+            <> 
+                <nav>
+                    <div className="nav-wrapper teal darken-4">
+                        <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <a href="#" className="brand-logo">RantBox</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><Link to="/dashboard">Dashboard</Link></li>
+                            <li><Link to="/rants">Your Rants</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/signin">Sign In</Link></li>
+                            <li><Link to="/signup">Sign up</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <ul id="slide-out" className="sidenav teal lighten-4">
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/rants">Your Rants</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/signin">Sign In</Link></li>
+                    <li><Link to="/signup">Sign up</Link></li>
+                </ul>
+            
+            </>
+        )
+    }
+}
+
+export default Header
+
+/*
+<nav className="nav-extended teal darken-4">
             <div className="nav-wrapper">
             <li className="brand-logo hide-on-med-and-down"><Link to="/dashboard">RantBox</Link></li>
             <ul id="nav-mobile" className="right">
@@ -16,8 +56,4 @@ const Header = () => {
             </ul>
             </div>
         </nav>
-        </>
-    )
-}
-
-export default Header
+*/
--- src/components/layout/posts/postCard.js
+++ src/components/layout/posts/postCard.js
@@ -4,9 +4,12 @@ const PostCard = (props) => {
     return (
             <div className="col s12 m6">
                 <div className="card teal lighten-5">
-                    <div className="card-content black-text">
-                        <span className="card-title"><strong>{props.title}</strong></span>
+                    <div className="card-content">
+                        <span className="card-title teal-text"><strong>{props.title}</strong></span>
                         <p>{props.text.substring(0, 25) + "..."}</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#" className="teal-text">view</a>
                     </div>
                 </div>
             </div>
