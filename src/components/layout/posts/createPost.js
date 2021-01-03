b17d26dfb4ce3ba6f43f68077466269744e36d8b
anuraj <anurajguha@gmail.com>
2021-01-03 19:38:10 +0530

    add validationa for createPost inputs

--- src/components/layout/posts/createPost.js
+++ src/components/layout/posts/createPost.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { isEmpty } from '../../../utils/utils'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const CreatePost = () => {
@@ -8,57 +11,74 @@ const CreatePost = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const postData = {
+            'title': title,
+            'text': body,
+            'feeling_level': feeling
+        }
+
+        if(isEmpty(title) || isEmpty(body) || isEmpty(feeling)) {
+            toast.error('Fields cannot be empty', {
+                position: "top-center",
+                autoClose: 2000,
+            })
+            return
+        }
+
+        console.log(postData)
     }
 
-
     return (
-        <div className="container" style={{'marginTop': '30px', 'marginBottom': '30px'}}>
-            <div className="card">
-                <form className="col s12">
-                    
-                    <div className="card-content">
-                        <span className="card-title teal-text"><strong>Create Rant</strong></span>
-                    </div>
-                    <div className="card-content">
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">create</i>
-                            <label htmlFor="title">Title</label>
-                            <input id="title" type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        <>  
+            <ToastContainer />
+            <div className="container" style={{'marginTop': '30px', 'marginBottom': '30px'}}>
+                <div className="card">
+                    <form className="col s12">
+                        
+                        <div className="card-content">
+                            <span className="card-title teal-text"><strong>Create Rant</strong></span>
+                        </div>
+                        <div className="card-content">
+                            <div className="input-field col s12">
+                                <i className="material-icons prefix">create</i>
+                                <label htmlFor="title">Title</label>
+                                <input id="title" type="text" value={title} onChange={e => setTitle(e.target.value)} />
+                            </div>
+
+                            <div className="input-field col s12">
+                                <i className="material-icons prefix">keyboard</i>
+                                <label htmlFor="text">Body</label>
+                                <textarea 
+                                    id="text" className="materialize-textarea" 
+                                    value={body} onChange={e => setBody(e.target.value)}
+                                ></textarea>
+                            </div>
+
                         </div>
 
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">keyboard</i>
-                            <label htmlFor="text">Body</label>
-                            <textarea 
-                                id="text" className="materialize-textarea" 
-                                value={body} onChange={e => setBody(e.target.value)}
-                            ></textarea>
+                        <div className="card-action">
+                            <label className="card-title"><strong>How are you feeling?</strong></label>
+                            <div className="input-field col s12">
+                                <select className="browser-default" defaultValue="" onChange={(e) => setFeeling(e.target.value)} >
+                                    <option value="" disabled>Choose</option>
+                                    <option value="Very Sad">Very Sad</option>
+                                    <option value="Sad">Sad</option>
+                                    <option value="Neutral">Neutral</option>
+                                    <option value="Pissed">Pissed</option>
+                                    <option value="Extremely Pissed">Extremely Pissed</option>
+                                    <option value="Fucking Furious">Fucking Furious</option>
+                                </select>
+                            </div>
                         </div>
 
-                    </div>
-
-                    <div className="card-action">
-                        <label className="card-title"><strong>How are you feeling?</strong></label>
-                        <div className="input-field col s12">
-                            <select className="browser-default" defaultValue="" onChange={(e) => setFeeling(e.target.value)} >
-                                <option value="" disabled>Choose</option>
-                                <option value="Very Sad">Very Sad</option>
-                                <option value="Sad">Sad</option>
-                                <option value="Neutral">Neutral</option>
-                                <option value="Pissed">Pissed</option>
-                                <option value="Extremely Pissed">Extremely Pissed</option>
-                                <option value="Fucking Furious">Fucking Furious</option>
-                            </select>
+                        <div className="card-content">
+                            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Create</button>
                         </div>
-                    </div>
 
-                    <div className="card-content">
-                        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Create</button>
-                    </div>
-
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
