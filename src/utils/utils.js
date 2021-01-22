0349f4ccfb8e45421b43ff83f71b21128e0c3c6f
anuraj <anurajguha@gmail.com>
2021-01-22 21:53:56 +0530

    remove feelings hardcode from utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,13 +2,4 @@ export const isEmpty = (s) => {
     return s.trim() === ''  
 }
 
-export const feelingsDict = {
-    'N': 'Neutral',
-    'FF': 'Fucking Furious',
-    'P': 'Pissed',
-    'EP': 'Extremely Pissed',
-    'S': 'Sad',
-    'VS': 'Very Sad'
-}
-
 export const maxBodyLength = 2000
