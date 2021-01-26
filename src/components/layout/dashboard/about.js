20eb9ca81b439ae07f8fa3d9424f8a65dda49deb
anuraj <anurajguha@gmail.com>
2021-01-26 14:23:03 +0530

    remove footer from about component

--- src/components/layout/dashboard/about.js
+++ src/components/layout/dashboard/about.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSpring, animated } from 'react-spring'
-import Footer from '../footer/footer'
 
 
 const About = () => {
@@ -42,7 +41,6 @@ const About = () => {
 
                 </div>
             </div>
-            <Footer />
         </animated.div>
     )
 }
